7dfa900e4514486793ff07c6d64fd8a599187888
Cy Ber (سايبر) <162600878+cyberman-cm@users.noreply.github.com>
2024-06-01 21:04:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// JS CODE
+
+const isOnline = navigator.onLine;
+const loaderElement = document.querySelector('.loader');
+
+loaderElement.style.display = isOnline ? "grid" : "none";
+
+window.addEventListener("load", () => {
+  loaderElement.style.display = "none"; 
+});
+
+// Debug, true;
+// Crx, true;
+// it's me, @cyberman.cm;
